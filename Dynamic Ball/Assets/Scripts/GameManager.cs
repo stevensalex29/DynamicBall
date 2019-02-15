765eb86d58ed4aedee519cf3616d404aa6f5afb2
stevensalex29 <stevensalex29@gmail.com>
2019-02-15 10:21:20 -0500

    added game manager

---
+++ Dynamic Ball/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //Attributes
+    private List<string> levels;
+    private int currentLevel;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        Scene currentScene = SceneManager.GetActiveScene(); //reset playerPrefs if at starting level
+        string sceneName = currentScene.name;
+        //if (sceneName == "Level1Tutorial")
+        //{
+        //    PlayerPrefs.DeleteAll();
+        //}
+
+        if (PlayerPrefs.HasKey("currentLevel"))
+            currentLevel = PlayerPrefs.GetInt("currentLevel"); //setup current level in playerprefs
+        else
+            PlayerPrefs.SetInt("currentLevel", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Loads the next level scene
+    public void nextLevel()
+    {
+        currentLevel++;
+    
+        if (currentLevel == levels.Count)
+        {
+            PlayerPrefs.SetInt("currentLevel", 0);
+        }
+
+        else
+        {
+            PlayerPrefs.SetInt("currentLevel", currentLevel);
+            SceneManager.LoadScene(levels[currentLevel]);
+        }
+    }
+}
