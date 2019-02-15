5470a48e776c4c6b8a2b26e4f34167cd30ef5422
stevensalex29 <stevensalex29@gmail.com>
2019-02-15 14:34:53 -0500

    next build, added ui and restart

--- Dynamic Ball/Assets/Scripts/ButtonScript.cs
+++ Dynamic Ball/Assets/Scripts/ButtonScript.cs
@@ -16,8 +16,27 @@ public class ButtonScript : MonoBehaviour
         
     }
 
+    // Start the game
     public void StartGame() {
         GameObject.FindGameObjectWithTag("StartLine").SetActive(false);
+        gameObject.SetActive(false);
+        GameObject.Find("Restart").SetActive(true);
+        GameObject.Find("HowToText").SetActive(false);
+        GameObject.Find("RestartText").SetActive(false);
+        GameObject.Find("StartText").SetActive(false);
+        GameObject.Find("Background").SetActive(false);
+    }
+
+    // Restart the game
+    public void RestartGame()
+    {
+        GameObject.Find("PlayerBall").transform.SetPositionAndRotation(new Vector3(0.5762594f, 9.607f, -11.205f), Quaternion.identity);
+        GameObject.Find("Start").SetActive(true);
+        GameObject.Find("HowToText").SetActive(true);
+        GameObject.Find("RestartText").SetActive(true);
+        GameObject.Find("StartText").SetActive(true);
+        GameObject.Find("Background").SetActive(true);
+        GameObject.FindGameObjectWithTag("StartLine").SetActive(true);
         gameObject.SetActive(false);
     }
 }
