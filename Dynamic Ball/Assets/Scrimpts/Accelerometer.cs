3c5ff7d0c7dd258b15b8b1630de08952822f4bac
DuncanLord <dxl1435@g.rit.edu>
2019-02-15 10:32:34 -0500

    Scrimpt done

--- Dynamic Ball/Assets/Scrimpts/Accelerometer.cs
+++ Dynamic Ball/Assets/Scrimpts/Accelerometer.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Accelerometer : MonoBehaviour
-{
+public class Accelerometer : MonoBehaviour{
+
+    public float speed = 10.0f;
+    Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
+    }
+
+    private void FixedUpdate(){
+
+        Vector3 acc = Input.acceleration;
+
+        rb.AddForce(acc.x * speed, 0, acc.y * speed);
     }
 }
