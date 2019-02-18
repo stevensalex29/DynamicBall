e6ab6723a09135128a9a7d62dbe663afcab18c95
Tung Nguyen (RIT Student) <thn4185@ad.rit.edu>
2019-02-18 10:51:57 -0500

    Added sample goal scene.

---
+++ Dynamic Ball/Assets/Scripts/PCMoveScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCMoveScript : MonoBehaviour
+{
+    Rigidbody rgbd;
+    float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translateHorz = Input.GetAxis("Horizontal") * speed;
+        float translateVert = Input.GetAxis("Vertical") * speed;
+
+        rgbd.AddForce(translateHorz, 0, translateVert);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Goal")
+        {
+            col.gameObject.SetActive(false);
+        }
+    }
+}
