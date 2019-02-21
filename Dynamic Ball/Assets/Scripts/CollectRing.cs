2bc3039792ef947220acd2d3b6379bd2bacaf381
Will <Will@WILLSPC>
2019-02-21 14:26:12 -0400

    Added rings, and fixed the collision with them and the ball

--- Dynamic Ball/Assets/Scripts/CollectRing.cs
+++ Dynamic Ball/Assets/Scripts/CollectRing.cs
@@ -10,13 +10,22 @@ public class CollectRing : MonoBehaviour
     void Start()
     {
         coinCounter = 0;
-        GameObject.Find("CoinCounter").GetComponent<UnityEngine.UI.Text>().text = "Coins: " + coinCounter.ToString();
+        GameObject.Find("CoinCounter").GetComponent<UnityEngine.UI.Text>().text = "Rings: " + coinCounter.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Ring")
+        {
+            Destroy(collision.gameObject);
+            coinCounter++;
+            GameObject.Find("CoinCounter").GetComponent<UnityEngine.UI.Text>().text = "Coins: " + coinCounter.ToString();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
