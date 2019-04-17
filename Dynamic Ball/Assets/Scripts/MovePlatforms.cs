6d0459b2afeb700f129ea0e8691947b670fc8f87
stevensalex29 <stevensalex29@gmail.com>
2019-04-17 12:22:10 -0400

    added moving and rotating platform mechanic

---
+++ Dynamic Ball/Assets/Scripts/MovePlatforms.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatforms : MonoBehaviour
+{
+    public float stopTime;
+    public float moveSpeed;
+    public float xDistance;
+    public float yDistance;
+    public float zDistance;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Vector3 stopPosition;
+    private bool stopped;
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+        endPosition = new Vector3(startPosition.x + xDistance, startPosition.y + yDistance, startPosition.z + zDistance);
+        stopPosition = endPosition;
+        stopped = false;
+        currentTime = stopTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move platform towards destination
+        if(gameObject.transform.position != stopPosition)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, stopPosition, moveSpeed * Time.deltaTime);
+        }
+        else // stop moving when at destination
+        {
+            stopped = true;
+        }
+
+        // decrement timer while stopped
+        if (stopped)
+        {
+            currentTime -= Time.deltaTime;
+            // when timer hits zero, reset attributes, toggle stop position for platform to move to
+            if(currentTime <= 0)
+            {
+                currentTime = stopTime;
+                stopped = false;
+                if (stopPosition == endPosition) stopPosition = startPosition;
+                else stopPosition = endPosition;
+            }
+        }
+            
+        
+    }
+}
---
+++ Dynamic Ball/Assets/Scripts/RotatePlatforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatforms : MonoBehaviour
+{
+    public float degreesPerSecondX;
+    public float degreesPerSecondY;
+    public float degreesPerSecondZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(degreesPerSecondX * Time.deltaTime, degreesPerSecondY * Time.deltaTime, degreesPerSecondZ * Time.deltaTime));
+    }
+}
