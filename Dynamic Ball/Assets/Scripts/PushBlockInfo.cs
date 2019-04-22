be99c20272b4a4f86cf9edf937a422dcb11d604a
Jake <jxt8029@rit.edu>
2019-04-22 00:05:54 -0400

    added easy push block level 2, tweaked level 1, updated pushblockinfo script to check for wall clipping

--- Dynamic Ball/Assets/Scripts/PushBlockInfo.cs
+++ Dynamic Ball/Assets/Scripts/PushBlockInfo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PushBlockInfo : MonoBehaviour
 {
     public Vector3 startPos;
+    public float maxZ;
 
     // Start is called before the first frame update
     void Start()
@@ -15,5 +16,9 @@ public class PushBlockInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.z > maxZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+        }
     }
 }
