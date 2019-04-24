51be1f0ed195cbcac839e53e4661faee3ee5954c
stevensalex29 <stevensalex29@gmail.com>
2019-04-24 11:24:38 -0400

    added keystore and fixed levels up

--- Dynamic Ball/Assets/Scripts/MenuManager.cs
+++ Dynamic Ball/Assets/Scripts/MenuManager.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class EndGameManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Rings").GetComponent<Text>().text = "Total Rings Collected: " + PlayerPrefs.GetInt("totalRings");
+        PlayerPrefs.DeleteAll();
     }
 
     // Update is called once per frame
