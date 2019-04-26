89c042a512b331469f81cd594f8d8998a5843bec
DuncanLord <dxl1435@g.rit.edu>
2019-04-26 10:53:30 -0400

    Color stuff

---
+++ Dynamic Ball/Assets/Scripts/ColorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    public int red;
+    public int green;
+    public int blue;
+
+    public Material mat;
+
+    private GameObject[] objectList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (red < 255) {
+            red = 255;
+        }
+        if (green < 255)
+        {
+            green = 255;
+        }
+        if (blue < 255)
+        {
+            blue = 255;
+        }
+
+
+        objectList = GameObject.FindGameObjectsWithTag("changeColor");
+
+        for (int i = 0; i < objectList.Length; i++) {
+            objectList[i].GetComponent<Renderer>().material = mat;
+        }
+    }
+}
