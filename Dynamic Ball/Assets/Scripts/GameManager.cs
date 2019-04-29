1c4073e7fbf04e0275a2df2a9fd5826890b82111
stevensalex29 <stevensalex29@gmail.com>
2019-04-29 14:31:19 -0400

    put levels in order, fixed ui description, and added color scheme

--- Dynamic Ball/Assets/Scripts/GameManager.cs
+++ Dynamic Ball/Assets/Scripts/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        levels = new List<string> {"TutorialScene","RegularTutorial1", "RegularTutorial2","RegularTutorial3","PushTutorial1","PushTutorial2","MoveTutorial1","MoveTutorial2","RotateTutorial1","RotateTutorial2","RoadBlocks","HotSwap","Pusher","VerticalFall","Plinko","TN_FerrisWheel" };
+        levels = new List<string> {"TutorialScene","RegularTutorial1", "RegularTutorial2","PushTutorial1","PushTutorial2","MoveTutorial1","MoveTutorial2","RotateTutorial1","RotateTutorial2", "Plinko","PushALot","Pusher","TN_Ferry","FerrisWheel","RoadBlocks","HotSwap","VerticalFall","ThinPlatforms"};
         Scene currentScene = SceneManager.GetActiveScene(); //reset playerPrefs if at starting level
         string sceneName = currentScene.name;
         sName = sceneName;
